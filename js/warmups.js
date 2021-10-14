d165af98d244916dfb212927b27f676275b4debf
Derek Dobbs <derek.tate.dobbs@gmail.com>
2021-10-14 09:18:17 -0500

    Submitting morning warmup

--- js/warmups.js
+++ js/warmups.js
@@ -1,7 +1,7 @@
 (function () {
     "use strict";
     //Warm up - 13 OCT 21
-
+    console.log("Warmup - 13 OCT 21");
     //Write a function that returns the number 7
     function returnSeven () {
         return 7;
@@ -15,4 +15,18 @@
 
     console.log("----------------------------------------------------------------------------------------------------")
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Warm-up - 14 OCT 21
+    console.log("Warmup - 14 OCT 21");
+/*
+* Copy and paste the following code:
+	var hadBreakfast = Boolean(Math.round(Math.random()));
+* Write a ternary expression to set a variable of hadBreakfastMessage to a string expressing whether or not you have had breakfast. You will use the variable hadBreakfast as your condition.
+
+* */
+    var hadBreakfast = Boolean(Math.round(Math.random()));
+    var hadBreakfastMessage = (hadBreakfast) ? "Yes, I had breakfast." : "No, I did not have breakfast.";
+    console.log(hadBreakfastMessage);
+
+    console.log("----------------------------------------------------------------------------------------------------")
+
 })();
