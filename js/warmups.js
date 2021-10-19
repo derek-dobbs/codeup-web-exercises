33c2b1e77ffe509382f8bbb7cdc90c4bbc422ee7
Derek Dobbs <derek.tate.dobbs@gmail.com>
2021-10-19 09:09:26 -0500

    Completed Morning Warmup Exercise

--- js/warmups.js
+++ js/warmups.js
@@ -55,6 +55,25 @@
     console.log(typeCheck(4));
     console.log("----------------------------------------------------------------------------------------------------")
 
-    //Warmup - October 18, 2021
+    //Warmup - October 19, 2021
+    /*
+    Write a program that console.logs the numbers from 1 to 100. But for multiples of three console.log “Fizz” instead
+    of the number and for the multiples of five console.log “Buzz”.
+    For numbers which are multiples of both three and five console.log “FizzBuzz”.
+
+     */
+    console.log("FizzBuzz Exercise");
+    for (let i = 1; i <= 100 ; i++) {
+        if(i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        }else if (i % 3 === 0) {
+            console.log("Fizz");
+        }else if (i % 5 === 0) {
+            console.log("Buzz");
+        }else {
+            console.log(i);
+        }
+    }
+    console.log("----------------------------------------------------------------------------------------------------")
 
 })();
