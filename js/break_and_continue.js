b32516e47d6d1088db53180e50e721f2c609f092
Derek Dobbs <derek.tate.dobbs@gmail.com>
2021-10-19 08:12:32 -0500

    Corrected break and continue exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -3,24 +3,27 @@
 
     //Break and Continue Exercise
     console.log("Break and Continue Exercise");
-    /*
-        Need to add: Use a loop and a break statement to continue prompting the user if they enter invalid input.
-     */
-    // console.log("Correction Needed \(see above comments\ in source code)");
 
-    let skipNumber = parseInt(prompt("Enter an odd number between 1 and 50."));
+    let skipNumber;
+    do {
+        skipNumber = parseInt(prompt("Enter an odd number between 1 and 50."));
+        if(skipNumber % 2 === 0 || skipNumber > 50 || skipNumber < 1){
+            skipNumber = false;
+        }
+    }while(!skipNumber || isNaN(skipNumber));
+
 
     console.log("Number to skip is: " + skipNumber);
     console.log("");
-        for (let i = 1; i <= 50 ; i++) {
-            if(i == skipNumber) {
-                console.log("Yikes! Skipping number " + skipNumber);
-                continue;
-            }
-            if(i % 2 !== 0) {
-                console.log("Here is an odd number: " + i);
-            }
+    for (let i = 1; i <= 50 ; i++) {
+        if(i == skipNumber) {
+            console.log("Yikes! Skipping number " + skipNumber);
+            continue;
         }
+        if(i % 2 !== 0) {
+            console.log("Here is an odd number: " + i);
+        }
+    }
         console.log("------------------------------------------------------------------------------------------------");
 
 })();
