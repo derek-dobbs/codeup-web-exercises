6d0b5008553896e7619454704ac5a13449e67d08
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2021-10-23 12:57:46 -0500

    Beginning uniqueInOrder exercise

--- js/practice-exercises.js
+++ js/practice-exercises.js
@@ -22,7 +22,19 @@
     console.log("Should return 811181: " + squareDigits(9119));
     console.log("Should return 9416: " + squareDigits(324));
 
+    //Additional solutions: https://www.codewars.com/kata/546e2562b03326a88e000020/solutions/javascript
     console.log("----------------------------------------------------------------------------------------------------");
+
+    //Source: codewars.com
+    // Implement the function unique_in_order which takes as argument a sequence and returns a list of items without
+    // any elements with the same value next to each other and preserving the original order of elements.
+    // For example:
+    // uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+    // uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+    // uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+    var uniqueInOrder = iterable => {
+        //your code here - remember iterable can be a string or an array
+    }
 
 
 })();
