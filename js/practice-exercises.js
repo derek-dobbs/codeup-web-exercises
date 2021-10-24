b906b45f47828e5ec85f6c89421e85b8ca64abf5
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2021-10-23 21:36:51 -0500

    Started twoSum exercise

--- js/practice-exercises.js
+++ js/practice-exercises.js
@@ -53,4 +53,35 @@
     //additional solutions: https://www.codewars.com/kata/54e6533c92449cc251001667/solutions/javascript
     console.log("----------------------------------------------------------------------------------------------------");
 
+    //Source: leetcode.com
+    // Two Sum
+    // Given an array of integers nums and an integer target, return indices of the two numbers such that
+    // they add up to target.
+    // You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    // You can return the answer in any order.
+    // Example 1:
+    // Input: nums = [2,7,11,15], target = 9
+    // Output: [0,1]
+    // Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+    //
+    // Example 2:
+    // Input: nums = [3,2,4], target = 6
+    // Output: [1,2]
+    //
+    // Example 3:
+    // Input: nums = [3,3], target = 6
+    // Output: [0,1]
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    console.log("Two Sum Exercise");
+    function twoSum(array, target) {
+
+    }
+
+    console.log("Expected output: [0,1], Output: " + twoSum([2,7,11,15], 9));
+    console.log("Expected output: [1,2], Output: " + twoSum([3,2,4], 6));
+    console.log("Expected output: [0,1], Output: " + twoSum([3,3], 6));
+    console.log("----------------------------------------------------------------------------------------------------");
+
+
+
 })();
