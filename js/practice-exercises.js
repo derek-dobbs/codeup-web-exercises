813d0249ade3452b3dc3a9d042b635ebaa12c88d
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2021-10-31 13:02:16 -0500

    Completed defangIPaddr exercise

--- js/practice-exercises.js
+++ js/practice-exercises.js
@@ -349,9 +349,12 @@
         Constraints:
         The given address is a valid IPv4 address.
      */
-    function defangIPaddr () {
-
+    function defangIPaddr (address) {
+        return  address.split('.').join('[.]');
     }
 
+    console.log(defangIPaddr("1.1.1.1"));
+    console.log(defangIPaddr("255.100.50.0"));
+    //all tests passed in leetcode
 
 })();
