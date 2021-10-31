4568f617409327a3e8a284ea2299a027e336ee0c
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2021-10-30 19:58:08 -0500

    Updated exercise

--- js/practice-exercises.js
+++ js/practice-exercises.js
@@ -263,6 +263,29 @@
     // console.log(triArea(7, 4));
     // console.log(triArea(10, 10));
     console.log("----------------------------------------------------------------------------------------------------");
+    //source: edabit
+    /*
+    * Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+
+    Examples
+    minMax([1, 2, 3, 4, 5]) ➞ [1, 5]
+
+    minMax([2334454, 5]) ➞ [5, 2334454]
+
+    minMax([1]) ➞ [1, 1]
+    Notes
+    All test arrays will have at least one element and are valid.
+    *
+    * */
+    function minMax (array) {
+        console.log(Math.min(...array));
+    }
+
+    minMax([3, 1, 2, 5, 4]);
+    minMax([2334454, 5]);
+    // console.log(minMax([1, 2, 3, 4, 5]));
+    // console.log(minMax([2334454, 5]));
+    // console.log(minMax([1]));
 
     console.log("----------------------------------------------------------------------------------------------------");
 
