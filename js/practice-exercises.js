915a397db32aa562fd3e2bbbd7eb67c8625f2667
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2021-10-30 19:29:44 -0500

    Completed exercise

--- js/practice-exercises.js
+++ js/practice-exercises.js
@@ -237,8 +237,35 @@
     // console.log(calcAge(20));
 
     console.log("----------------------------------------------------------------------------------------------------");
+    //source: edabit, https://edabit.com/challenge/3CaszbdZYGN4otQD8
+    /*Area of a Triangle
+    *
+    *Write a function that takes the base and height of a triangle and return its area.
+
+    Examples
+    triArea(3, 2) ➞ 3
+
+    triArea(7, 4) ➞ 14
+
+    triArea(10, 10) ➞ 50
+    Notes
+    The area of a triangle is: (base * height) / 2
+    Don't forget to return the result.
+    If you get stuck on a challenge, find help in the Resources tab.
+    If you're really stuck, unlock solutions in the Solutions tab.
+    *
+    * */
+    function triArea(base, height) {
+        return (base * height) / 2;
+    }
+    // console.log("TriArea Exercise:")
+    // console.log(triArea(3, 2));
+    // console.log(triArea(7, 4));
+    // console.log(triArea(10, 10));
+    console.log("----------------------------------------------------------------------------------------------------");
 
     console.log("----------------------------------------------------------------------------------------------------");
+
 
 
 })();
