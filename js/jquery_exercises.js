a8e67ec4532a777d843e2d4e55b50d97e590173f
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2021-11-19 11:34:24 -0600

    Updated exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
+    // alert( 'The DOM has finished loading!' );
+
+    // $('*').css('border', '1px solid #F00');
+
+    $('body').css('background-color', '#D3D3D3');
+    $('.paragraph').css('background-color', 'yellow');
+    $('#main-heading').css('color', 'blue');
+    $('.paragraph').css('color', 'purple');
+    $('span').css('font-style', 'italic');
+
 });
