dc31d471150bb7982c1692546dbf213f46112ba9
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2021-11-19 11:49:11 -0600

    Updated exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // alert( 'The DOM has finished loading!' );
 
     // $('*').css('border', '1px solid #F00');
 
-    $('body').css('background-color', '#D3D3D3');
-    $('.paragraph').css('background-color', 'yellow');
-    $('#main-heading').css('color', 'blue');
-    $('.paragraph').css('color', 'purple');
-    $('span').css('font-style', 'italic');
+    // $('body').css('background-color', '#D3D3D3');
+    // $('.paragraph').css('background-color', 'yellow');
+    // $('#main-heading').css('color', 'blue');
+    // $('.paragraph').css('color', 'purple');
+    // $('span').css('font-style', 'italic');
+    //
+    // //Use jQuery to select an element by the id. Alert the contents of the element.
+    // var contents = $('#paragraph-1').html();
+    // alert(contents);
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 
 });
