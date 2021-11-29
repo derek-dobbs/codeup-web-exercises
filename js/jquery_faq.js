578a4b1e4936ddf01cc30b66c23acc9032c793f8
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2021-11-29 12:06:42 -0600

    Completed exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,4 +1,9 @@
 "use strict";
 $(document).ready(function() {
     $('dd').addClass('invisible');
+
+    $('#show-answers').click(function (event) {
+       event.preventDefault();
+        $('dd').toggleClass('invisible');
+    });
 }); //end document.ready
