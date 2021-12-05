2c10dd5113115fc1e1baf52730ea7dffab5f1efc
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2021-12-04 20:14:07 -0600

    Updated code

--- js/mapbox-geocoder-utils.js
+++ js/mapbox-geocoder-utils.js
@@ -19,10 +19,13 @@ function geocode(search, token) {
     return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
         .then(function(res) {
             return res.json();
-            // to get all the data from the request, comment out the following three lines...
-        }).then(function(data) {
+
+        })
+        // to get all the data from the request, comment out the following three lines...
+        .then(function(data) {
             return data.features[0].center;
-        });
+        })
+        ;
 }
 
 
@@ -47,7 +50,7 @@ function reverseGeocode(coordinates, token) {
             return res.json();
         })
         // to get all the data from the request, comment out the following three lines...
-        .then(function(data) {
-            return data.features[0].place_name;
-        });
+        // .then(function(data) {
+        //     return data.features[0].place_name;
+        // });
 }
