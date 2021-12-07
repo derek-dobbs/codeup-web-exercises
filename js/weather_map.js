36391c225789b61bc64778f58487c8cf3c06ddc6
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2021-12-06 19:44:51 -0600

    Refactored code

--- js/weather_map.js
+++ js/weather_map.js
@@ -33,6 +33,7 @@ $(document).ready(function () {
         units: "imperial",
         APPID: OPEN_WEATHER_KEY
     }).done(function (data) {
+        console.log(data);
         $('#current-temperature').html(parseInt(data.current.temp) + "&#176");
         $('#current-description').html(data.current.weather[0].description);
         $('#current-feels-like').html("<em>Feels like</em>: " + parseInt(data.current.feels_like) + "&#176");
@@ -40,57 +41,89 @@ $(document).ready(function () {
 
         $('#current-icon').attr("src", "http://openweathermap.org/img/w/" + data.current.weather[0].icon + ".png");
 
-        $('#day0').html(new Date(data.daily[0].dt * 1000));
-        $('#day1').html(new Date(data.daily[1].dt * 1000));
-        $('#day2').html(new Date(data.daily[2].dt * 1000));
-        $('#day3').html(new Date(data.daily[3].dt * 1000));
-        $('#day4').html(new Date(data.daily[4].dt * 1000));
+        // let dayOfWeek = new Date(data.daily[0].dt * 1000).getDay();
+        // let month = new Date(data.daily[0].dt * 1000).getMonth();
+        // let day = new Date(data.daily[0].dt * 1000).getDate();
+        // let year = new Date(data.daily[0].dt * 1000).getFullYear();
+        //
+        // switch (dayOfWeek) {
+        //     case 0:
+        //         dayOfWeek = "Sun";
+        //         break;
+        //     case 1:
+        //         dayOfWeek = "Mon";
+        //         break;
+        //     case 2:
+        //         dayOfWeek = "Tue";
+        //         break;
+        //     case 3:
+        //         dayOfWeek = "Wed";
+        //         break;
+        //     case 4:
+        //         dayOfWeek = "Thu";
+        //         break;
+        //     case 5:
+        //         dayOfWeek = "Fri";
+        //         break;
+        //     case 6:
+        //         dayOfWeek = "Sat";
+        //         break;
+        // }
+        //
+        // switch (month) {
+        //     case 0:
+        //         month = "Jan";
+        //         break;
+        //     case 1:
+        //         month = "Feb";
+        //         break;
+        //     case 2:
+        //         month = "Mar";
+        //         break;
+        //     case 3:
+        //         month = "Apr";
+        //         break;
+        //     case 4:
+        //         month = "May";
+        //         break;
+        //     case 5:
+        //         month = "Jun";
+        //         break;
+        //     case 6:
+        //         month = "Jul";
+        //         break;
+        //     case 7:
+        //         month = "Aug";
+        //         break;
+        //     case 8:
+        //         month = "Sep";
+        //         break;
+        //     case 9:
+        //         month = "Oct";
+        //         break;
+        //     case 10:
+        //         month = "Nov";
+        //         break;
+        //     case 11:
+        //         month = "Dec";
+        //         break;
+        //
+        // }
+        //
+        // let date = dayOfWeek + " " + month + " " + day + ", " + year;
+        //
+        // console.log(date);
 
-        $('#day0-low').html("Low " + parseInt(data.daily[0].temp.min) + "&#176");
-        $('#day0-high').html("High " + parseInt(data.daily[0].temp.max) + "&#176");
-
-        $('#day1-low').html("Low " + parseInt(data.daily[1].temp.min) + "&#176");
-        $('#day1-high').html("High " + parseInt(data.daily[1].temp.max) + "&#176");
-
-        $('#day2-low').html("Low " + parseInt(data.daily[2].temp.min) + "&#176");
-        $('#day2-high').html("High " + parseInt(data.daily[2].temp.max) + "&#176");
-
-        $('#day3-low').html("Low " + parseInt(data.daily[3].temp.min) + "&#176");
-        $('#day3-high').html("High " + parseInt(data.daily[3].temp.max) + "&#176");
-
-        $('#day4-low').html("Low " + parseInt(data.daily[4].temp.min) + "&#176");
-        $('#day4-high').html("High " + parseInt(data.daily[4].temp.max) + "&#176");
-
-
-        $("#day0-icon").attr("src", "http://openweathermap.org/img/w/" + data.daily[0].weather[0].icon + ".png");
-        $("#day1-icon").attr("src", "http://openweathermap.org/img/w/" + data.daily[1].weather[0].icon + ".png");
-        $("#day2-icon").attr("src", "http://openweathermap.org/img/w/" + data.daily[2].weather[0].icon + ".png");
-        $("#day3-icon").attr("src", "http://openweathermap.org/img/w/" + data.daily[3].weather[0].icon + ".png");
-        $("#day4-icon").attr("src", "http://openweathermap.org/img/w/" + data.daily[4].weather[0].icon + ".png");
-
-        $('#day0-description').html(data.daily[0].weather[0].description);
-        $('#day1-description').html(data.daily[1].weather[0].description);
-        $('#day2-description').html(data.daily[2].weather[0].description);
-        $('#day3-description').html(data.daily[3].weather[0].description);
-        $('#day4-description').html(data.daily[4].weather[0].description);
-
-        $('#day0-humidity').html(parseInt(data.daily[0].humidity));
-        $('#day1-humidity').html(parseInt(data.daily[1].humidity));
-        $('#day2-humidity').html(parseInt(data.daily[2].humidity));
-        $('#day3-humidity').html(parseInt(data.daily[3].humidity));
-        $('#day4-humidity').html(parseInt(data.daily[4].humidity));
-
-        $('#day0-wind').html(parseFloat(data.daily[0].wind_speed));
-        $('#day1-wind').html(parseFloat(data.daily[1].wind_speed));
-        $('#day2-wind').html(parseFloat(data.daily[2].wind_speed));
-        $('#day3-wind').html(parseFloat(data.daily[3].wind_speed));
-        $('#day4-wind').html(parseFloat(data.daily[4].wind_speed));
-
-        $('#day0-pressure').html(parseInt(data.daily[0].pressure));
-        $('#day1-pressure').html(parseInt(data.daily[1].pressure));
-        $('#day2-pressure').html(parseInt(data.daily[2].pressure));
-        $('#day3-pressure').html(parseInt(data.daily[3].pressure));
-        $('#day4-pressure').html(parseInt(data.daily[4].pressure));
+        for (let i = 0; i < 6; i++) {
+            $('#day' + i).html(new Date(data.daily[i].dt * 1000));
+            $('#day' + i + '-low').html("Low " + parseInt(data.daily[i].temp.min) + "&#176");
+            $('#day' + i + "-high").html("High " + parseInt(data.daily[i].temp.max) + "&#176");
+            $('#day' + i + "-icon").attr("src", "http://openweathermap.org/img/w/" + data.daily[i].weather[0].icon + ".png");
+            $('#day' + i + "-description").html(data.daily[i].weather[0].description);
+            $('#day' + i + "-humidity").html(parseInt(data.daily[i].humidity));
+            $('#day' + i + "-wind").html(parseFloat(data.daily[i].wind_speed));
+            $('#day' + i + "-pressure").html(parseInt(data.daily[i].pressure));
+        }
 
         $('#change-location-link').click(function () {
             $('#location-prompt').removeClass('visibility-hidden')
@@ -124,63 +157,16 @@ $(document).ready(function () {
                     // console.log(location);
                 });
 
-                $('#current-temperature').html(parseInt(data.current.temp) + "&#176");
-                $('#current-description').html(data.current.weather[0].description);
-                $('#current-feels-like').html("<em>Feels like</em>: " + parseInt(data.current.feels_like) + "&#176");
-                $('#wind-speed').html("Wind Speed: " + parseInt(data.current.wind_speed));
-
-                $('#current-icon').attr("src", "http://openweathermap.org/img/w/" + data.current.weather[0].icon + ".png");
-
-                $('#day0').html(new Date(data.daily[0].dt * 1000));
-                $('#day1').html(new Date(data.daily[1].dt * 1000));
-                $('#day2').html(new Date(data.daily[2].dt * 1000));
-                $('#day3').html(new Date(data.daily[3].dt * 1000));
-                $('#day4').html(new Date(data.daily[4].dt * 1000));
-
-                $('#day0-low').html("Low " + parseInt(data.daily[0].temp.min) + "&#176");
-                $('#day0-high').html("High " + parseInt(data.daily[0].temp.max) + "&#176");
-
-                $('#day1-low').html("Low " + parseInt(data.daily[1].temp.min) + "&#176");
-                $('#day1-high').html("High " + parseInt(data.daily[1].temp.max) + "&#176");
-
-                $('#day2-low').html("Low " + parseInt(data.daily[2].temp.min) + "&#176");
-                $('#day2-high').html("High " + parseInt(data.daily[2].temp.max) + "&#176");
-
-                $('#day3-low').html("Low " + parseInt(data.daily[3].temp.min) + "&#176");
-                $('#day3-high').html("High " + parseInt(data.daily[3].temp.max) + "&#176");
-
-                $('#day4-low').html("Low " + parseInt(data.daily[4].temp.min) + "&#176");
-                $('#day4-high').html("High " + parseInt(data.daily[4].temp.max) + "&#176");
-
-                $("#day0-icon").attr("src", "http://openweathermap.org/img/w/" + data.daily[0].weather[0].icon + ".png");
-                $("#day1-icon").attr("src", "http://openweathermap.org/img/w/" + data.daily[1].weather[0].icon + ".png");
-                $("#day2-icon").attr("src", "http://openweathermap.org/img/w/" + data.daily[2].weather[0].icon + ".png");
-                $("#day3-icon").attr("src", "http://openweathermap.org/img/w/" + data.daily[3].weather[0].icon + ".png");
-                $("#day4-icon").attr("src", "http://openweathermap.org/img/w/" + data.daily[4].weather[0].icon + ".png");
-
-                $('#day0-description').html(data.daily[0].weather[0].description);
-                $('#day1-description').html(data.daily[1].weather[0].description);
-                $('#day2-description').html(data.daily[2].weather[0].description);
-                $('#day3-description').html(data.daily[3].weather[0].description);
-                $('#day4-description').html(data.daily[4].weather[0].description);
-
-                $('#day0-humidity').html(parseInt(data.daily[0].humidity));
-                $('#day1-humidity').html(parseInt(data.daily[1].humidity));
-                $('#day2-humidity').html(parseInt(data.daily[2].humidity));
-                $('#day3-humidity').html(parseInt(data.daily[3].humidity));
-                $('#day4-humidity').html(parseInt(data.daily[4].humidity));
-
-                $('#day0-wind').html(parseFloat(data.daily[0].wind_speed));
-                $('#day1-wind').html(parseFloat(data.daily[1].wind_speed));
-                $('#day2-wind').html(parseFloat(data.daily[2].wind_speed));
-                $('#day3-wind').html(parseFloat(data.daily[3].wind_speed));
-                $('#day4-wind').html(parseFloat(data.daily[4].wind_speed));
-
-                $('#day0-pressure').html(parseInt(data.daily[0].pressure));
-                $('#day1-pressure').html(parseInt(data.daily[1].pressure));
-                $('#day2-pressure').html(parseInt(data.daily[2].pressure));
-                $('#day3-pressure').html(parseInt(data.daily[3].pressure));
-                $('#day4-pressure').html(parseInt(data.daily[4].pressure));
+                for (let i = 0; i < 6; i++) {
+                    $('#day' + i).html(new Date(data.daily[i].dt * 1000));
+                    $('#day' + i + '-low').html("Low " + parseInt(data.daily[i].temp.min) + "&#176");
+                    $('#day' + i + "-high").html("High " + parseInt(data.daily[i].temp.max) + "&#176");
+                    $('#day' + i + "-icon").attr("src", "http://openweathermap.org/img/w/" + data.daily[i].weather[0].icon + ".png");
+                    $('#day' + i + "-description").html(data.daily[i].weather[0].description);
+                    $('#day' + i + "-humidity").html(parseInt(data.daily[i].humidity));
+                    $('#day' + i + "-wind").html(parseFloat(data.daily[i].wind_speed));
+                    $('#day' + i + "-pressure").html(parseInt(data.daily[i].pressure));
+                }
             });
         });
 
@@ -219,56 +205,16 @@ $(document).ready(function () {
 
                     $('#current-icon').attr("src", "http://openweathermap.org/img/w/" + data.current.weather[0].icon + ".png");
 
-                    $('#day0').html(new Date(data.daily[0].dt * 1000));
-                    $('#day1').html(new Date(data.daily[1].dt * 1000));
-                    $('#day2').html(new Date(data.daily[2].dt * 1000));
-                    $('#day3').html(new Date(data.daily[3].dt * 1000));
-                    $('#day4').html(new Date(data.daily[4].dt * 1000));
-
-                    $('#day0-low').html("Low " + parseInt(data.daily[0].temp.min) + "&#176");
-                    $('#day0-high').html("High " + parseInt(data.daily[0].temp.max) + "&#176");
-
-                    $('#day1-low').html("Low " + parseInt(data.daily[1].temp.min) + "&#176");
-                    $('#day1-high').html("High " + parseInt(data.daily[1].temp.max) + "&#176");
-
-                    $('#day2-low').html("Low " + parseInt(data.daily[2].temp.min) + "&#176");
-                    $('#day2-high').html("High " + parseInt(data.daily[2].temp.max) + "&#176");
-
-                    $('#day3-low').html("Low " + parseInt(data.daily[3].temp.min) + "&#176");
-                    $('#day3-high').html("High " + parseInt(data.daily[3].temp.max) + "&#176");
-
-                    $('#day4-low').html("Low " + parseInt(data.daily[4].temp.min) + "&#176");
-                    $('#day4-high').html("High " + parseInt(data.daily[4].temp.max) + "&#176");
-
-                    $("#day0-icon").attr("src", "http://openweathermap.org/img/w/" + data.daily[0].weather[0].icon + ".png");
-                    $("#day1-icon").attr("src", "http://openweathermap.org/img/w/" + data.daily[1].weather[0].icon + ".png");
-                    $("#day2-icon").attr("src", "http://openweathermap.org/img/w/" + data.daily[2].weather[0].icon + ".png");
-                    $("#day3-icon").attr("src", "http://openweathermap.org/img/w/" + data.daily[3].weather[0].icon + ".png");
-                    $("#day4-icon").attr("src", "http://openweathermap.org/img/w/" + data.daily[4].weather[0].icon + ".png");
-
-                    $('#day0-description').html(data.daily[0].weather[0].description);
-                    $('#day1-description').html(data.daily[1].weather[0].description);
-                    $('#day2-description').html(data.daily[2].weather[0].description);
-                    $('#day3-description').html(data.daily[3].weather[0].description);
-                    $('#day4-description').html(data.daily[4].weather[0].description);
-
-                    $('#day0-humidity').html(parseInt(data.daily[0].humidity));
-                    $('#day1-humidity').html(parseInt(data.daily[1].humidity));
-                    $('#day2-humidity').html(parseInt(data.daily[2].humidity));
-                    $('#day3-humidity').html(parseInt(data.daily[3].humidity));
-                    $('#day4-humidity').html(parseInt(data.daily[4].humidity));
-
-                    $('#day0-wind').html(parseFloat(data.daily[0].wind_speed));
-                    $('#day1-wind').html(parseFloat(data.daily[1].wind_speed));
-                    $('#day2-wind').html(parseFloat(data.daily[2].wind_speed));
-                    $('#day3-wind').html(parseFloat(data.daily[3].wind_speed));
-                    $('#day4-wind').html(parseFloat(data.daily[4].wind_speed));
-
-                    $('#day0-pressure').html(parseInt(data.daily[0].pressure));
-                    $('#day1-pressure').html(parseInt(data.daily[1].pressure));
-                    $('#day2-pressure').html(parseInt(data.daily[2].pressure));
-                    $('#day3-pressure').html(parseInt(data.daily[3].pressure));
-                    $('#day4-pressure').html(parseInt(data.daily[4].pressure));
+                    for (let i = 0; i < 6; i++) {
+                        $('#day' + i).html(new Date(data.daily[i].dt * 1000));
+                        $('#day' + i + '-low').html("Low " + parseInt(data.daily[i].temp.min) + "&#176");
+                        $('#day' + i + "-high").html("High " + parseInt(data.daily[i].temp.max) + "&#176");
+                        $('#day' + i + "-icon").attr("src", "http://openweathermap.org/img/w/" + data.daily[i].weather[0].icon + ".png");
+                        $('#day' + i + "-description").html(data.daily[i].weather[0].description);
+                        $('#day' + i + "-humidity").html(parseInt(data.daily[i].humidity));
+                        $('#day' + i + "-wind").html(parseFloat(data.daily[i].wind_speed));
+                        $('#day' + i + "-pressure").html(parseInt(data.daily[i].pressure));
+                    }
                 });
             });
         });
