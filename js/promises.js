800888781959f9c0545a67cec9210b32335f3e16
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2021-12-09 18:59:30 -0600

    began Promises exercise

---
+++ js/promises.js
@@ -0,0 +1,3 @@
+//TODO: Create a file named promises.js inside of your js directory and link it to an HTML file of your choice.
+//TODO: Generate a Personal Access Token for the GitHub API.
+//TODO: Create a function that accepts a GitHub username, and returns a promise that resolves returning just the date of the last commit that user made. Reference the github api documentation to achieve this.
