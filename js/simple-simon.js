69bb7a93bdb2d99c54961102d2cb351d18b10168
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2021-12-29 02:12:34 -0600

    Updated HTML, CSS, and JS

--- js/simple-simon.js
+++ js/simple-simon.js
@@ -1,5 +1,11 @@
 "use strict";
 (function () {
     // alert("Test simple-simon.js");
+    $(document).ready(function() {
+        $('#reset-button').click(function (event) {
+            event.preventDefault();
 
+            $('.box').toggleClass('bg-color-none');
+        }); // end $('#reset-button').click
+    }); //end $(document).ready
 })();
