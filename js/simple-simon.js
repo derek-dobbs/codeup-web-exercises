961b0c482ed2afd89de276b8bc029f93bd8d50ee
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2021-12-29 02:18:58 -0600

    Added listeners to boxes

--- js/simple-simon.js
+++ js/simple-simon.js
@@ -2,6 +2,22 @@
 (function () {
     // alert("Test simple-simon.js");
     $(document).ready(function() {
+        $('#green-box').click(function () {
+            alert("Green box was clicked");
+        }); // end $('#green-box').click
+
+        $('#red-box').click(function () {
+            alert("Red box was clicked");
+        }); // end $('#red-box').click
+
+        $('#yellow-box').click(function () {
+            alert("Yellow box was clicked");
+        }); // end $('#yellow-box').click
+
+        $('#blue-box').click(function () {
+            alert("Blue box was clicked");
+        }); // end $('#blue-box').click
+
         $('#reset-button').click(function (event) {
             event.preventDefault();
 
