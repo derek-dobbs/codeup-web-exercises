021e16445c190a67fa75f8b6b702747cffc2bbef
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2021-12-29 03:22:56 -0600

    Updated HTML and JS

--- js/simple-simon.js
+++ js/simple-simon.js
@@ -2,6 +2,8 @@
 (function () {
     // alert("Test simple-simon.js");
     $(document).ready(function() {
+        $('.box').toggleClass('bg-color-none');
+
         $('#green-box').click(function () {
             alert("Green box was clicked");
         }); // end $('#green-box').click
@@ -23,5 +25,56 @@
 
             $('.box').toggleClass('bg-color-none');
         }); // end $('#reset-button').click
+
+        function flashSequence () {
+            $('.button').css('visibility', 'hidden');
+            $('h1').css('visibility', 'hidden');
+            setTimeout(function () {
+                $('#green-box').toggleClass('bg-color-none');
+            },1000);
+
+            setTimeout(function () {
+                $('#green-box').toggleClass('bg-color-none');
+            },1500);
+
+            setTimeout(function () {
+                $('#red-box').toggleClass('bg-color-none');
+            },2000);
+
+            setTimeout(function () {
+                $('#red-box').toggleClass('bg-color-none');
+            },2500);
+
+            setTimeout(function () {
+                $('#blue-box').toggleClass('bg-color-none');
+            },3000);
+
+            setTimeout(function () {
+                $('#blue-box').toggleClass('bg-color-none');
+            },3500);
+
+            setTimeout(function () {
+                $('#yellow-box').toggleClass('bg-color-none');
+            },4000);
+
+            setTimeout(function () {
+                $('#yellow-box').toggleClass('bg-color-none');
+            },4500);
+
+            setTimeout(function () {
+                $('.box').toggleClass('bg-color-none');
+            }, 5000);
+
+            setTimeout(function () {
+                $('h1').css('visibility', 'visible');
+                $('.button').css('visibility', 'visible');
+            }, 5000);
+        } // end flashSequence
+
+        // flashSequence();
+
+        $('#start-button').click(function () {
+            // alert("Start button clicked");
+        });
     }); //end $(document).ready
 })();
